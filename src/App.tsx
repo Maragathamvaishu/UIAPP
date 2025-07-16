9667e1a49d55f6c3f897a883ebf3096a2056e165
Maragathamvaishu <maragatham122002@gmail.com>
2025-07-16 14:15:50 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './header';
+
+function App() {
+  return (
+   <div>
+    <h1 className="font-size text-2xl font-bold text-black-500 ">Top Resources:</h1>
+    <h6 className="relative top-2 ">Access real-life data, fully organized and ready to use, all in one place.</h6><br />
+     <Header />
+    </div>
+    
+  
+    
+  );
+}
+
+export default App;
